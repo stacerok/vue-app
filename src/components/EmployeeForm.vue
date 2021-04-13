38b98a68ef3b3a8df452830bf1169afaa9b6e458
Stacie Taitague <stacie.taitague@gmail.com>
2021-04-13 13:38:31 -0400

    vue tutorial

---
+++ src/components/EmployeeForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="employee-form">
+    <form>
+      <label>Employee name</label>
+      <input v-model="employee.name" type="text" />
+      <label>Employee Email</label>
+      <input v-model="employee.email" type="text" />
+      <form @submit.prevent="handleSubmit">
+        <button>Add Employee</button>
+      </form>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "employee-form",
+  data() {
+    return {
+      employee: {
+        name: "",
+        email: "",
+      },
+    };
+  },
+  methods: {
+    handleSubmit() {
+      console.log("testing handleSubmit"), this.$emit("add:employee", this.employee);
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  margin-bottom: 2rem;
+}
+</style>
